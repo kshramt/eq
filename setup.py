94cee8476d479b1765644b034c09754c276a9c6c
kshramt <thisisdummy@example.com>
2013-06-15 13:18:52 +0900

    Add required files

---
+++ setup.py
@@ -0,0 +1,17 @@
+import distutils.core
+distutils.core.setup(name='eq',
+                     version='0.0.0',
+                     author='kshramt',
+                     author_email='thisisdummy@example.com',
+                     url='http://www.thisisdummy.example.com',
+                     description='Earthquake science library.',
+                     long_description='Handy utilities for earthquake science.',
+                     packages=['eq'],
+                     classifiers=["Development Status :: 1 - Planning",
+                                  "Environment :: Console",
+                                  "Intended Audience :: Education",
+                                  "Intended Audience :: Science/Research",
+                                  "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                                  "Operating System :: Unix",
+                                  "Programming Language :: Python :: 3",
+                                  "Topic :: Scientific/Engineering",])
