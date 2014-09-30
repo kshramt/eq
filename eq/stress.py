0d5ced296a2f52184017956d44f24f3e797e7f12
kshramt <thisisdummy@example.com>
2014-10-01 00:26:43 +0900

    Optimize

--- eq/stress.py
+++ eq/stress.py
@@ -2,7 +2,7 @@
 
 
 import unittest
-# import sys
+import sys
 from math import sin, cos, acos
 # import re
 # import pickle
@@ -34,20 +34,66 @@ def normal_from_strike_dip(strike, dip):
 
 def from_alpha_beta_gamma_phi(alpha, beta, gamma, phi):
     assert 0 <= phi <= 1
-    D = np.array(((1e0, 0e0, 0e0),
-                  (0e0, (2e0*phi - 1e0)/(2e0 - phi), 0e0),
-                  (0e0, 0e0, -(1e0 + phi)/(2e0 - phi))))
-    R_alpha = np.array(((cos(alpha), -sin(alpha), 0e0),
-                        (sin(alpha), cos(alpha), 0e0),
-                        (0e0, 0e0, 1e0)))
-    R_beta = np.array(((cos(beta), 0e0, -sin(beta)),
-                       (0e0, 1e0, 0e0),
-                       (sin(beta), 0e0, cos(beta))))
-    R_gamma = np.array(((1e0, 0e0, 0e0),
-                        (0e0, cos(gamma), -sin(gamma)),
-                        (0e0, sin(gamma), cos(gamma))))
-    R = eq.util.dots(R_alpha, R_beta, R_gamma)
+    D = ((1e0, 0e0, 0e0),
+         (0e0, (2e0*phi - 1e0)/(2e0 - phi), 0e0),
+         (0e0, 0e0, -(1e0 + phi)/(2e0 - phi)))
+    R = _R_alpha_beta_gamma(alpha, beta, gamma)
     return eq.util.dots(R, D, R.T)
+
+
+def _R_alpha_beta_gamma(alpha, beta, gamma):
+    Ca = cos(alpha)
+    Sa = sin(alpha)
+    Cb = cos(beta)
+    Sb = sin(beta)
+    Cg = cos(gamma)
+    Sg = sin(gamma)
+    return np.array(((Ca*Cb, -(Sa*Cg + Sb*Sg*Ca), Sa*Sg - Sb*Ca*Cg),
+                     (Sa*Cb, Ca*Cg - Sa*Sb*Sg, -(Sa*Sb*Cg + Sg*Ca)),
+                     (Sb, Sg*Cb, Cb*Cg)))
+
+def _R_alpha(x):
+    c = cos(x)
+    s = sin(x)
+    return np.array(((c, -s, 0e0),
+                     (s, c, 0e0),
+                     (0e0, 0e0, 1e0)))
+
+
+def _R_beta(x):
+    c = cos(x)
+    s = sin(x)
+    return np.array(((c, 0e0, -s),
+                     (0e0, 1e0, 0e0),
+                     (s, 0e0, c)))
+
+def _R_gamma(x):
+    c = cos(x)
+    s = sin(x)
+    return np.array(((1e0, 0e0, 0e0),
+                     (0e0, c, -s),
+                     (0e0, s, c)))
+
+
+def _test():
+    import numpy.testing as npt
+
+    _tests = []
+    def _t(f):
+        _tests.append(f)
+
+    @_t
+    def t():
+        alpha = 0.1
+        beta = 0.2
+        gamma = 0.3
+        npt.assert_almost_equal(_R_alpha_beta_gamma(alpha, beta, gamma),
+                                eq.util.dots(_R_alpha(alpha), _R_beta(beta), _R_gamma(gamma)))
+
+    for t in _tests:
+        t()
+        print('.', end='', file=sys.stderr)
+    print(file=sys.stderr)
 
 
 class _Tester(unittest.TestCase):
@@ -57,4 +103,5 @@ class _Tester(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    _test()
     unittest.main()
