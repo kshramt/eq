6f9a74c56b5475d1e7f6798869c7976cf6306e45
kshramt <thisisdummy@example.com>
2014-10-02 18:08:34 +0900

    Return angles in radian, not in degree

--- eq/fdt.py
+++ eq/fdt.py
@@ -3,6 +3,9 @@
 """
 
 
+import unittest
+
+import eq.kshramt
 def load(fp):
     return (parse_record(line.rstrip('\n')) for line in fp)
 
@@ -10,17 +13,34 @@ def load(fp):
 def parse_record(line):
     f_az, f_pl, s_az, s_pl, faulting_type, *comment = line.split(maxsplit=5)
     faulting_type = faulting_type.lower()
-    assert faulting_type in ('n', 'r', 'd', 's')
-    ret = dict(f_az=float(f_az), f_pl=float(f_pl), s_az=float(s_az), s_pl=float(s_pl), faulting_type=faulting_type)
+    assert faulting_type in FAULTING_TYPES
+    ret = dict(f_az=eq.kshramt.rad(float(f_az)),
+               f_pl=eq.kshramt.rad(float(f_pl)),
+               s_az=eq.kshramt.rad(float(s_az)),
+               s_pl=eq.kshramt.rad(float(s_pl)),
+               faulting_type=faulting_type)
     if comment:
         ret['comment'] = comment[0]
     return ret
 
 
-def test():
-    assert parse_record('10.0 -20 30 40 N') == {'f_az': 10.0, 'f_pl': -20.0, 's_az': 30.0, 's_pl': 40.0, 'faulting_type': 'n'}
-    assert parse_record('10.0 -20 30 40 N a comment') == {'f_az': 10.0, 'f_pl': -20.0, 's_az': 30.0, 's_pl': 40.0, 'faulting_type': 'n', 'comment': 'a comment'}
+class _Tester(unittest.TestCase):
+
+    def test_parse_record(self):
+        self.assertAlmostEqual(parse_record('10.0 -20 30 40 N'),
+                               dict(f_az=eq.kshramt.rad(10),
+                                    f_pl=eq.kshramt.rad(-20),
+                                    s_az=eq.kshramt.rad(30),
+                                    s_pl=eq.kshramt.rad(40),
+                                    faulting_type='n'))
+        self.assertAlmostEqual(parse_record('10.0 -20 30 40 N a comment'),
+                               dict(f_az=eq.kshramt.rad(10),
+                                    f_pl=eq.kshramt.rad(-20),
+                                    s_az=eq.kshramt.rad(30),
+                                    s_pl=eq.kshramt.rad(40),
+                                    faulting_type='n',
+                                    comment='a comment'))
 
 
 if __name__ == '__main__':
-    test()
+    unittest.main()
